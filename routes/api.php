0e89ac7f4b8559dbb33f2f5f3628af4646c3dd54
Matheus Luna <srmatheus6162@gmail.com>
2024-10-01 15:03:44 -0300

    adjusting the routes

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/users', [UserController::class, 'store']);
 Route::post('/login', [AuthController::class, 'store']);
-Route::get('/verifyToken', [AuthController::class, "show"]);
+Route::get('/token', [AuthController::class, 'show']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/users', UserController::class)->except('store');
-
-    Route::delete('/logout', [AuthController::class, "destroy"]);
+    Route::delete('/logout', [AuthController::class, 'destroy']);
     Route::resource('/posts', PostController::class);
     Route::apiResource('/likes', LikeController::class);
     Route::apiResource('/follow', FollowerController::class);
