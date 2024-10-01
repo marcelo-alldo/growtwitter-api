28a3660f2a35bb40b7310e61d7c7420874b7a58d
Matheus Luna <srmatheus6162@gmail.com>
2024-10-01 16:51:38 -0300

    remove a import

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 
